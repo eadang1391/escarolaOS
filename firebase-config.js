a5e94581b31457b2b156a469e299b0ebd59987e6
eadang1391 <eadang@gmail.com>
2026-02-02 10:33:49 -0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // Importar las librerías necesarias
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, 
+import { 
+    getFirestore, 
     collection, 
     addDoc, 
     getDocs, 
@@ -9,7 +10,8 @@ import { getFirestore,
     doc, 
     getDoc, 
     updateDoc, 
-    deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+    deleteDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqnPu5uInhoWBTwPs46asfm4ecotShfHY",
@@ -20,7 +22,20 @@ const firebaseConfig = {
   appId: "1:211000066025:web:d71791f51acb571414d578"
 };
 
-// Inicializar Firebase y exportar para usar en el index
+// 1. Inicializar
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { db, collection, addDoc, getDocs, query, where, doc, getDoc, updateDoc, deleteDoc };
+const db = getFirestore(app);
+
+// 2. EXPORTAR TODO JUNTO (Sin duplicados)
+export { 
+    db, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    where, 
+    doc, 
+    getDoc, 
+    updateDoc, 
+    deleteDoc 
+};
