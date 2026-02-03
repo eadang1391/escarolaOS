ba1295fca806a22f718e42a036c0734146bfd47d
eadang1391 <eadang@gmail.com>
2026-02-03 13:23:01 -0600

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,7 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    query, 
-    where, 
-    doc, 
-    deleteDoc, 
-    orderBy, 
-    limit 
+    getFirestore, collection, addDoc, getDocs, query, where, 
+    doc, deleteDoc, updateDoc, setDoc, orderBy, limit 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -25,16 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// ESTO ES VITAL: Asegúrate de que todas estas funciones estén exportadas
 export { 
-    db, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    query, 
-    where, 
-    doc, 
-    deleteDoc, 
-    orderBy, 
-    limit 
+    db, collection, addDoc, getDocs, query, where, 
+    doc, deleteDoc, updateDoc, setDoc, orderBy, limit 
 };
